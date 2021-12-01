4955e894244da0565bb95691680c68c04ef5d246
Maxime Legare <mleg1234@protonmail.com>
2021-12-01 14:10:45 -0500

    strings, bool, numbers, [], {}, tuple, enum, any

---
+++ basics.ts
@@ -0,0 +1,18 @@
+//types in Ts
+
+// number
+// string
+// boolean
+
+function add(n1: number, n2: number){
+   console.log(n1 + n2)     
+}
+
+const number1 = 5
+let number2 = 3
+
+// number2 = "hello"
+// cannot reassign with different type.
+
+add(number1, number2)
+
